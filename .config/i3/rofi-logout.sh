379e32f3b87d441f49263a0b45d99f2dd9a02200
Michael Chunko <34099159+MikeChunko@users.noreply.github.com>
2020-04-16 14:14:58 -0400

    Suspend, exit using i3-msg

--- .config/i3/rofi-logout.sh
+++ .config/i3/rofi-logout.sh
@@ -2,10 +2,12 @@
 
 # Remember to give it exec priviledge, or chmod +x /path/to/power.sh
 
-chosen=$(echo -e "Logout\nShutdown\nReboot" | rofi -dmenu -i)
+chosen=$(echo -e "Logout\nSuspend\nShutdown\nReboot" | rofi -dmenu -i)
 
 if [[ $chosen = "Logout" ]]; then
-	jwm -exit
+	i3-msg exit
+elif [[ $chosen = "Suspend" ]]; then
+	systemctl suspend
 elif [[ $chosen = "Shutdown" ]]; then
 	systemctl poweroff
 elif [[ $chosen = "Reboot" ]]; then
