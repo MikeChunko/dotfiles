a6ce262352c3d495dface0f3caf883b6b118c531
Michael Chunko <34099159+MikeChunko@users.noreply.github.com>
2020-05-14 09:50:10 -0400

    Lock when suspending

--- .config/i3/rofi-logout.sh
+++ .config/i3/rofi-logout.sh
@@ -7,7 +7,7 @@ chosen=$(echo -e "Logout\nSuspend\nShutdown\nReboot" | rofi -dmenu -i)
 if [[ $chosen = "Logout" ]]; then
 	i3-msg exit
 elif [[ $chosen = "Suspend" ]]; then
-	systemctl suspend
+	systemctl suspend && i3lockr --blur 200 -- --nofork --ignore-empty-password
 elif [[ $chosen = "Shutdown" ]]; then
 	systemctl poweroff
 elif [[ $chosen = "Reboot" ]]; then
