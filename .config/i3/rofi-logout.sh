e71160d2021969bedde321f4fc6d2ac381f81530
Michael Chunko <34099159+MikeChunko@users.noreply.github.com>
2020-05-18 11:00:30 -0400

    Conversion from bash to sh

--- .config/i3/rofi-logout.sh
+++ .config/i3/rofi-logout.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-# Remember to give it exec priviledge, or chmod +x /path/to/power.sh
+# Remember to give it exec priviledge, or chmod +x /path/to/rofi-logout.sh
 
 chosen=$(echo -e "Logout\nSuspend\nShutdown\nReboot" | rofi -dmenu -i)
 
-if [[ $chosen = "Logout" ]]; then
+if [ $chosen = "Logout" ]; then
 	i3-msg exit
-elif [[ $chosen = "Suspend" ]]; then
+elif [ $chosen = "Suspend" ]; then
 	systemctl suspend && i3lockr --blur 200 -- --nofork --ignore-empty-password
-elif [[ $chosen = "Shutdown" ]]; then
+elif [ $chosen = "Shutdown" ]; then
 	systemctl poweroff
-elif [[ $chosen = "Reboot" ]]; then
+elif [ $chosen = "Reboot" ]; then
 	systemctl reboot
 fi
